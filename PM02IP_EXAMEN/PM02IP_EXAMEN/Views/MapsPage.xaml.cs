2561cda11ea6c499a648b69f52876c7f5c644f91
Stephany2000 <calixstephany2000@gmail.com>
2021-10-15 04:07:04 -0600

    Mapa

---
+++ PM02IP_EXAMEN/PM02IP_EXAMEN/Views/MapsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms.Maps;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM02IP_EXAMEN.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapsPage : ContentPage
+    {
+        public MapsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
